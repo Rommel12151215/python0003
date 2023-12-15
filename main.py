37a187b2a565381d3d9e7bf808c2cb95fe8e3cbf
Rommel <karlsrommel255@gmail.com>
2023-12-15 13:28:20 -0600

    Python0003

---
+++ main.py
@@ -0,0 +1,14 @@
+print("""Todo en triple comillas dobles o simples
+        se vera igualmente reflejado incluso
+        si abarca mas de una linea de codigo""")
+
+print("nombre completo")
+print("fecha de hoy")
+
+print(""" I am signing up for Replit's 100 days of Python challenge!
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. No excuses for not coding from the middle of a field!""")
+
+print("i am feeling 😝")
+
+
